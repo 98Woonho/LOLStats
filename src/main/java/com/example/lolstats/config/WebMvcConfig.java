e3d6a6ff40956eeebf1ca636be0de3134303ec2d
98Woonho <9woonho8@gmail.com>
2025-01-28 19:00:14 +0900

    Riot API Key 발급 작업 중

--- src/main/java/com/example/lolstats/config/WebMvcConfig.java
+++ src/main/java/com/example/lolstats/config/WebMvcConfig.java
@@ -11,6 +11,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("file:src/main/resources/static/");
+        registry.addResourceHandler("/**").addResourceLocations("file:src/main/resources/static/txt/");
     }
 }
