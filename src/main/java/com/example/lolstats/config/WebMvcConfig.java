9f9d63ec269422c8cd19059714ab6f2c90fadf2a
98Woonho <9woonho8@gmail.com>
2025-01-28 19:30:30 +0900

    Riot API Key 발급 작업 중

---
+++ src/main/java/com/example/lolstats/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.example.lolstats.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/riot.txt")
+                .addResourceLocations("classpath:/BOOT-INF/classes/static/");  // 파일이 있는 정확한 경로
+    }
+}
+
